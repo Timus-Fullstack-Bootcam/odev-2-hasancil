cc2fa5f5738abd4631ecb5a2a2d23cb8a013bdd7
Hasan Çil <hsncil97@hotmail.com>
2023-11-19 14:02:54 +0300

    Ödev 3 - Daire Modüler Dosyası

---
+++ circle.js
@@ -0,0 +1,8 @@
+const circleArea = (radius) => Math.PI * radius ** 2;
+
+const circleCircumference = (radius) => 2 * Math.PI * radius;
+
+module.exports = {
+  circleArea,
+  circleCircumference,
+};
